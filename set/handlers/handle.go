80575ac537f085c0fa9233c7c68e67ff629efe41
cassiusbessa <cassiusbessa@gmail.com>
2023-03-29 16:52:00 -0300

    refactor: :art: change handleSet to call handle entities methods

---
+++ set/handlers/handle.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/cassiusbessa/create-text/entities"
+	"github.com/cassiusbessa/create-text/logs"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func Handle(c *gin.Context) {
+	defer logs.Elapsed("Set Text")()
+	db, sec := c.Param("company"), c.Param("section")
+	logrus.Warnf("Setting %v Text on %v", sec, db)
+	validSection := false
+	m := "Section must be: "
+	for _, s := range entities.DisplayedSections {
+		m += s + " "
+		if s == sec {
+			validSection = true
+			break
+		}
+	}
+	if !validSection {
+		logrus.Errorf("Invalid section %v", sec)
+		c.JSON(http.StatusBadRequest, gin.H{"error": m})
+		return
+	}
+	switch sec {
+	case "horary":
+		handleHorary(c, sec, db)
+		return
+	case "testimonials":
+		handleTestimonial(c, sec, db)
+		return
+	case "address":
+		handleAddress(c, sec, db)
+		return
+	case "contacts":
+		handleContacts(c, sec, db)
+		return
+	case "services":
+		handleServices(c, sec, db)
+		return
+	default:
+		handleDefaultText(c, sec, db)
+		return
+	}
+}
--- set/main.go
+++ set/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"github.com/cassiusbessa/create-text/handlers"
-	// "github.com/cassiusbessa/create-service/logs"
+	"github.com/cassiusbessa/create-text/logs"
 	"github.com/cassiusbessa/create-text/repositories"
 	"github.com/sirupsen/logrus"
 )
 
-// var file = logs.Init()
+var file = logs.Init()
 
 func main() {
-	// defer file.Close()
+	defer file.Close()
 	r := handlers.Router()
 	repositories.Repo.Ping()
-	r.PUT("/texts/:company/:section", handlers.Set)
-	// r.StaticFile("/logs", "./logs/logs.log")
+	r.PUT("/texts/:company/:section", handlers.Handle)
+	r.StaticFile("/logs", "./logs/logs.log")
 	if err := r.Run(":8080"); err != nil {
 		logrus.Fatalf("Failed to start server: %v", err)
 	}
